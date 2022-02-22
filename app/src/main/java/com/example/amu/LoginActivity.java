0d234912987d6b5b5db504a77fdf13dcec723706
peter munga <pngarimunga3488@gmail.com>
2022-02-22 14:42:14 +0300

    update

--- app/src/main/java/com/example/amu/LoginActivity.java
+++ app/src/main/java/com/example/amu/LoginActivity.java
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity {
         // Configure Google Sign In
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.def ault_web_client_id))
                 .requestEmail()
                 .build();
 
